070fb431e33d59aa3d052b532eebb05fa7a589b7
Zolto787 <tadumik@gmail.com>
2023-02-04 19:31:41 +0300

    console.log

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCadasdharm')
+    print_hi('PyCadasdadasdasdasdasdasdharm')
+    print("suck_cock")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
